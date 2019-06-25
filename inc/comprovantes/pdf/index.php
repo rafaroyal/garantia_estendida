ba3101247a6b89bb894247bc4558c28ad1bb099e
rafaroyal <rafaroyal.nogueira@gmail.com>
2019-06-25 16:49:20 -0300

    remove arquivos

--- inc/comprovantes/pdf/index.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-$origem = $_SERVER['HTTP_REFERER'];
-    if (empty($origem))
-    {
-        header("Location: http://www.fixou.com.br");
-    }
-?>
--- inc/comprovantes/pdf/pdf/teste/teste.php
+++
@@ -1,8 +0,0 @@
-<?php
-include("/home/vipassistenciaco/public_html/pdf/mpdf.php");
-// Saves file on the server as 'filename.pdf'
-$mpdf=new mPDF();
-$mpdf->WriteHTML('<p>Hallo World</p>');
-$mpdf->Output('filename.pdf','F');
-
-?>
